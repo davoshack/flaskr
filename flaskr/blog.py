4499894d96c3ca6eb135fb7645a4e01f96cd8048
Juan David H <juan.hernandez@anomali.com>
2020-08-17 17:04:57 -0500

    Added update method to blog

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -42,6 +42,34 @@ def create():
 
         return render_template('blog/create.html')
 
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                db.execute(
+                    'UPDATE pos SET title = ?, body = ?'
+                    ' WHERE id = ?',
+                    (title, body, id) 
+                )
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+    return render_template('blog/update.html', post=post)
+
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
